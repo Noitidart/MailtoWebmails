44322f876b173514008b757ffee9962049a6223f
Noitidart <noitidart@gmail.com>
2014-08-04 21:30:17 -0700

    1.0.rev16
    quitting jsm method, i just copied pasted the jsm part into xpcom.js and
    bootstrap.js. it fixes that weird unexplainable error i was getting in
    bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -8,7 +8,34 @@ const self = {
 
 Cu.import('resource://gre/modules/Services.jsm');
 Cu.import('resource://gre/modules/devtools/Console.jsm');
-Cu.import(self.path + 'modules/infoForWebmailHandlers.jsm');
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
+var myServices = {};
+
+XPCOMUtils.defineLazyGetter(myServices, 'eps', function(){ return Cc['@mozilla.org/uriloader/external-protocol-service;1'].getService(Ci.nsIExternalProtocolService) });
+XPCOMUtils.defineLazyGetter(myServices, 'hs', function(){ return Cc['@mozilla.org/uriloader/handler-service;1'].getService(Ci.nsIHandlerService) });
+
+const infoForWebmailHandlers = [
+	{
+		name: 'AIM Mail',
+		uriTemplate: 'http://webmail.aol.com/Mail/ComposeMessage.aspx?to=%s',
+		circleSelector: '.skills .css' //this is for me to target the proper row in the dom of the prefs frontend document.getElementById(circleId).parentNode.parentNode is the row element
+	},
+	{
+		name: 'GMail',
+		uriTemplate: 'https://mail.google.com/mail/?extsrc=mailto&url=%s',
+		circleSelector: '.skills .ai'
+	},
+	{
+		name: 'Outlook Live',
+		uriTemplate: 'https://mail.live.com/secure/start?action=compose&to=%s',
+		circleSelector: '.skills .ps'
+	},
+	{
+		name: 'Y! Mail',
+		uriTemplate: 'https://compose.mail.yahoo.com/?To=%s',
+		circleSelector: '.skills .html'
+	}
+];
 
 //start pref stuff
 //needs ES5, i dont know what min browser version of FF starts support for ES5
--- options/js/xpcom.js
+++ options/js/xpcom.js
@@ -1,6 +1,33 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import('resource://gre/modules/Services.jsm');
-Cu.import('chrome://mailtowebmails/content/modules/infoForWebmailHandlers.jsm');
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
+var myServices = {};
+
+XPCOMUtils.defineLazyGetter(myServices, 'eps', function(){ return Cc['@mozilla.org/uriloader/external-protocol-service;1'].getService(Ci.nsIExternalProtocolService) });
+XPCOMUtils.defineLazyGetter(myServices, 'hs', function(){ return Cc['@mozilla.org/uriloader/handler-service;1'].getService(Ci.nsIHandlerService) });
+
+const infoForWebmailHandlers = [
+	{
+		name: 'AIM Mail',
+		uriTemplate: 'http://webmail.aol.com/Mail/ComposeMessage.aspx?to=%s',
+		circleSelector: '.skills .css' //this is for me to target the proper row in the dom of the prefs frontend document.getElementById(circleId).parentNode.parentNode is the row element
+	},
+	{
+		name: 'GMail',
+		uriTemplate: 'https://mail.google.com/mail/?extsrc=mailto&url=%s',
+		circleSelector: '.skills .ai'
+	},
+	{
+		name: 'Outlook Live',
+		uriTemplate: 'https://mail.live.com/secure/start?action=compose&to=%s',
+		circleSelector: '.skills .ps'
+	},
+	{
+		name: 'Y! Mail',
+		uriTemplate: 'https://compose.mail.yahoo.com/?To=%s',
+		circleSelector: '.skills .html'
+	}
+];
 const INSTALL_HANDLER = 0;
 const UNINSTALL_HANDLER = 1;
 const SET_HANDLER_ACTIVE = 2;
